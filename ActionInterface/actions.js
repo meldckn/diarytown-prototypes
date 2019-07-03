adf051b51a22f9f518a2d0475c1de5291764e11e
Daniel Man <daniel.man9001@gmail.com>
2019-07-03 15:48:05 -0700

    Added my action interface.

---
+++ ActionInterface/actions.js
@@ -0,0 +1,514 @@
+var actions = [ 
+	{
+		category : "self",
+		group : "life of the mind",
+		name : "used-quality",
+		text : [ "used some quality of self" ],
+		related : [],
+		othercategories : ""
+	},{
+		category : "self",
+		group : "did stuff by myself",
+		name : "lazy",
+		text : [ "was lazy", "was unproductive", "had a mental health day", "was a lump on a log all day" ],
+		related : [],
+		othercategories : ""
+	},{
+		category : "self",
+		group : "negative",
+		name : "late",
+		text : [ "was late", "was late to #event" ],
+		related : [],
+		othercategories : "negative"
+	},{
+		category : "self",
+		group : "negative",
+		name : "was-mean",
+		text : [ "was mean", "was mean to #someone" ],
+		related : [],
+		othercategories : "negative"
+	},{
+		category : "self",
+		group : "did stuff by myself",
+		name : "productive",
+		text : [ "was productive" ],
+		related : [],
+		othercategories : "work"
+	},{
+		category : "self",
+		group : "did stuff by myself",
+		name : "tv",
+		text : [ "watched TV", "watched a show", "watched a movie" ],
+		related : [],
+		othercategories : ""
+	},{
+		category : "movement",
+		group : "went somewhere",
+		name : "went-home",
+		text : [ "went home" ],
+		related : [],
+		othercategories : ""
+	},{
+		category : "movement",
+		group : "went somewhere",
+		name : "vacationed",
+		text : [ "went on a holiday", "went on a day trip", "went on vacation" ] ,
+		related : [],
+		othercategories : ""
+	},{
+		category : "movement",
+		group : "went somewhere",
+		name : "adventured",
+		text : [ "went on a journey", "had an adventure" ] ,
+		related : [],
+		othercategories : ""
+	},{
+		category : "movement",
+		group : "went somewhere",
+		name : "dined-out",
+		text : [ "went out for food", "went out to dinner", "went out for drinks" ] ,
+		related : [],
+		othercategories : ""
+	},{
+		category : "movement",
+		group : "went somewhere",
+		name : "shopped",
+		text : [ "went shopping" ],
+		related : [],
+		othercategories : ""
+	},{
+		category : "work",
+		group : "went somewhere",
+		name : "work-meeting",
+		text : [ "went to a meeting" ],
+		related : [],
+		othercategories : ""
+	},{
+		category : "movement",
+		group : "went somewhere",
+		name : "went-to-class",
+		text : [ "went to a class" ] ,
+		related : [],
+		othercategories : "work"
+	},{
+		category : "social",
+		group : "went somewhere",
+		name : "went-to-party",
+		text : [ "went to a party" ],
+		related : [],
+		othercategories : "movement"
+	},{
+		category : "movement",
+		group : "went somewhere",
+		name : "went-to-sport",
+		text : [ "went to a sporting event" ],
+		related : [],
+		othercategories : ""
+	},{
+		category : "movement",
+		group : "went somewhere",
+		name : "went-to-doctor",
+		text : [ "went to the doctor", "went to the hospital" ],
+		related : [],
+		othercategories : ""
+	},{
+		category : "negative",
+		group : "negative",
+		name : "large-responsibility",
+		text : [ "a large weight of responsibility got dropped into my lap", "big responsibility is weighing on me" ],
+		related : [],
+		othercategories : "work"
+	},{
+		category : "self",
+		group : "life of the mind",
+		name : "avoided-responsibility",
+		text : [ "avoid doing something", "avoided a responsibility" ],
+		related : [],
+		othercategories : "work"
+	},{
+		category : "social",
+		group : "did social things",
+		name : "chatted-with-stranger",
+		text : [ "chatted with a stranger" ],
+		related : [],
+		othercategories : ""
+	},{
+		category : "social",
+		group : "did social things",
+		name : "chatted-with-someone",
+		text : [ "chatted with someone" ] ,
+		related : [],
+		othercategories : ""
+	},{
+		category : "social",
+		group : "did social things",
+		name : "worked-together",
+		text : [ "collaborated with someone", "worked together with someone" ],
+		related : [],
+		othercategories : "work"
+	},{
+		category : "self",
+		group : "did stuff by myself",
+		name : "did-nothing",
+		text : [ "did nothing", "couldn't do anything" ],
+		related : [],
+		othercategories : "negative"
+	},{
+		category : "self",
+		group : "did stuff by myself",
+		name : "daydreamed",
+		text : [ "daydreamed" ],
+		related : [],
+		othercategories : ""
+	},{
+		category : "self",
+		group : "did stuff by myself",
+		name : "meditated",
+		text : [ "meditated" ],
+		related : [],
+		othercategories : ""
+	},{
+		category : "self",
+		group : "did stuff by myself",
+		name : "chores",
+		text : [ "did chores", "took care of personal business", "did adult things" ],
+		related : [],
+		othercategories : "movement"
+	},{
+		category : "self",
+		group : "did stuff by myself",
+		name : "hobby",
+		text : [ "did hobby", "hobbied" ],
+		related : [],
+		othercategories : ""
+	},{
+		category : "social",
+		group : "did social things",
+		name : "helped-someone",
+		text : [ "did something for someone", "helped someone", "did a favor for someone" ],
+		related : [],
+		othercategories : ""
+	},{
+		category : "social",
+		group : "did social things",
+		name : "got-help",
+		text : [ "asked for help", "got help on something from someone" ],
+		related : [],
+		othercategories : "work"
+	},{
+		category : "self",
+		group : "did stuff by myself",
+		name : "exercised",
+		text : [ "exercised", "worked out" ],
+		related : [],
+		othercategories : "movement"
+	},{
+		category : "self",
+		group : "felt things",
+		name : "feeling-excited",
+		text : [ "feeling excited for something coming up" ] ,
+		related : [],
+		othercategories : ""
+	},{
+		category : "self",
+		group : "felt things",
+		name : "sick",
+		text : [ "felt sick", "felt bad" ],
+		related : [],
+		othercategories : "negative"
+	},{
+		category : "self",
+		group : "did stuff by myself",
+		name : "finished-work",
+		text : [ "finished some work" ],
+		related : [],
+		othercategories : ""
+	},{
+		category : "self",
+		group : "did stuff by myself",
+		name : "interneted",
+		text : [ "interneted", "puttered around on the internet", "messed around on the internet", "played on the computer" ] ,
+		related : [],
+		othercategories : ""
+	},{
+		category : "self",
+		group : "",
+		name : "full-day",
+		text : [ "had a busy day", "had a full day" ],
+		related : [],
+		othercategories : ""
+	},{
+		category : "self",
+		group : "life of the mind",
+		name : "good-idea",
+		text : [ "had a good idea" ],
+		related : [],
+		othercategories : "mental"
+	},{
+		category : "social",
+		group : "did social things",
+		name : "nice-convo",
+		text : [ "had a nice convo w/ someone" ],
+		related : [],
+		othercategories : ""
+	},{
+		category : "social",
+		group : "did social things",
+		name : "heard-from",
+		text : [ "heard from someone" ],
+		related : [],
+		othercategories : ""
+	},{
+		category : "social",
+		group : "did social things",
+		name : "resolved-conflict",
+		text : [ "helped resolve a fight" ],
+		related : [],
+		othercategories : ""
+	},{
+		category : "self",
+		group : "",
+		name : "hung-out",
+		text : [ "hung out" ],
+		related : [],
+		othercategories : "social"
+	},{
+		category : "social",
+		group : "did social things",
+		name : "hung-out-with",
+		text : [ "hung out with people", "spent quality time with someone" ],
+		related : [],
+		othercategories : ""
+	},{
+		category : "self",
+		group : "negative",
+		name : "hurt-self",
+		text : [ "hurt myself" ],
+		related : [],
+		othercategories : ""
+	},{
+		category : "self",
+		group : "life of the mind",
+		name : "made-plans",
+		text : [ "made plans" ],
+		related : [],
+		othercategories : ""
+	},{
+		category : "self",
+		group : "did stuff by myself",
+		name : "made-something",
+		text : [ "made something" ],
+		related : [],
+		othercategories : ""
+	},{
+		category : "self",
+		group : "negative",
+		name : "messed-up",
+		text : [ "messed up" ],
+		related : [],
+		othercategories : "negative"
+	},{
+		category : "self",
+		group : "life of the mind",
+		name : "missing-someone",
+		text : [ "missed someone", "wished someone was here", "missing someone" ],
+		related : [],
+		othercategories : ""
+	},{
+		category : "movement",
+		group : "went somewhere",
+		name : "moved",
+		text : [ "moved" ],
+		related : [],
+		othercategories : ""
+	},{
+		category : "movement",
+		group : "went somewhere",
+		name : "went-somewhere",
+		text : [ "went somewhere" ],
+		related : [],
+		othercategories : ""
+	},{
+		category : "social",
+		group : "did social things",
+		name : "got-recognition",
+		text : [ "people said nice things about me", "felt recognized", "got recognition" ],
+		related : [],
+		othercategories : ""
+	},{
+		category : "self",
+		group : "did stuff by myself",
+		name : "played-games",
+		text : [ "played games" ],
+		related : [],
+		othercategories : ""
+	},{
+		category : "social",
+		group : "did social things",
+		name : "ran-into",
+		text : [ "ran into someone" ],
+		related : [],
+		othercategories : ""
+	},{
+		category : "self",
+		group : "life of the mind",
+		name : "turning-point",
+		text : [ "reached a turning point in something" ],
+		related : [],
+		othercategories : ""
+	},{
+		category : "social",
+		group : "did social things",
+		name : "reached-out-to",
+		text : [ "reached out to someone" ],
+		related : [],
+		othercategories : ""
+	},{
+		category : "self",
+		group : "did stuff by myself",
+		name : "read",
+		text : [ "read a book", "read something" ] ,
+		related : [],
+		othercategories : ""
+	},{
+		category : "self",
+		group : "life of the mind",
+		name : "realized-something",
+		text : [ "realized something" ],
+		related : [],
+		othercategories : ""
+	},{
+		category : "self",
+		group : "did stuff by myself",
+		name : "saw-something",
+		text : [ "saw something", "encountered something" ],
+		related : [],
+		othercategories : ""
+	},{
+		category : "self",
+		group : "did stuff by myself",
+		name : "saw-art",
+		text : [ "saw art", "experienced art" ],
+		related : [],
+		othercategories : ""
+	},{
+		category : "self",
+		group : "did stuff by myself",
+		name : "learned",
+		text : [ "learned something new", "set out to learn something new" ],
+		related : [],
+		othercategories : ""
+	},{
+		category : "self",
+		group : "did stuff by myself",
+		name : "slept-in",
+		text : [ "slept in" ],
+		related : [],
+		othercategories : ""
+	},{
+		category : "social",
+		group : "negative",
+		name : "angry-with-me",
+		text : [ "someone got angry with me", "someone got upset with me" ],
+		related : [],
+		othercategories : ""
+	},{
+		category : "social",
+		group : "did social things",
+		name : "someone-did-something-cool",
+		text : [ "someone did something cool" ],
+		related : [],
+		othercategories : ""
+	},{
+		category : "social",
+		group : "negative",
+		name : "fight",
+		text : [ "someone and someone fought", "someone and someone got into a fight" ],
+		related : [],
+		othercategories : ""
+	},{
+		category : "social",
+		group : "negative",
+		name : "someone-bothered",
+		text : [ "someone bothered me", "someone got on my nerves", "someone annoyed me" ],
+		related : [],
+		othercategories : "self"
+	},{
+		category : "social",
+		group : "negative",
+		name : "someone-was-jerk",
+		text : [ "someone was a jerk to me", "someone was bitchy at me", "someone yelled at me", "someone got angry at me" ],
+		related : [],
+		othercategories : "negative"
+	},{
+		category : "self",
+		group : "did stuff by myself",
+		name : "studied",
+		text : [ "studied", "studied for a thing coming up", "studied for a test coming up" ],
+		related : [],
+		othercategories : "work"
+	},{
+		category : "self",
+		group : "life of the mind",
+		name : "reminisced",
+		text : [ "reminisced", "thought of an old memory", "thought of old times" ] ,
+		related : [],
+		othercategories : ""
+	},{
+		category : "movement",
+		group : "went somewhere",
+		name : "took-walk",
+		text : [ "took a walk", "took a walk to nowhere in particular", "took a drive" ],
+		related : [],
+		othercategories : ""
+	},{
+		category : "social",
+		group : "did social things",
+		name : "cared-for",
+		text : [ "took care of someone" ],
+		related : [],
+		othercategories : ""
+	},{
+		category : "self",
+		group : "did stuff by myself",
+		name : "treated-self",
+		text : [ "treated myself" ],
+		related : [],
+		othercategories : ""
+	},{
+		category : "external",
+		group : "stuff out of my control",
+		name : "power-outage",
+		text : [ "there was a power outage somewhere" ],
+		related : [],
+		othercategories : ""
+	},{
+		category : "external",
+		group : "stuff out of my control",
+		name : "storm",
+		text : [ "stormed", "there was a storm" ],
+		related : [],
+		othercategories : ""
+	},{
+		category : "self",
+		group : "did stuff by myself",
+		name : "cooked",
+		text : [ "cooked", "cooked something yummy" ],
+		related : [],
+		othercategories : ""
+	},{
+		category : "social",
+		group : "did social things",
+		name : "people-visited",
+		text : [ "had people over", "had someone over" ],
+		related : [ "visited-people" ],
+		othercategories : ""
+	},{
+		category : "social",
+		group : "went somewhere",
+		name : "visited-people",
+		text : [ "visited friends", "visited relatives" ],
+		related : [ "people-visited" ],
+		othercategories : "movement"
+	}
+];
---
+++ ActionInterface/script.js
@@ -0,0 +1,221 @@
+
+var categoryNames = [];
+
+
+for (let action of actions) {
+	
+	if (categoryNames.indexOf(action.category)===-1) {
+		categoryNames.push(action.category);
+	}
+}
+
+var categories = [];
+
+for (let categoryName of categoryNames) {
+	let category = {
+		name : categoryName,
+		actions : []
+	};
+	categories.push(category);
+}
+
+function closeCategory (actionButtonDiv) {
+	anime({
+		targets:'.actionButton',
+		opacity:0,
+		duration:2000
+	})
+	anime({
+		targets: '.actionButton',
+		translateX:-150,
+		duration: 2000
+	})
+	anime({
+		targets:'.actionTextDiv',
+		opacity:0,
+		duration:2000
+	})
+	anime({
+		targets: '.actionTextDiv',
+		translateX:-150,
+		duration: 2000
+	})
+	window.setTimeout(function(){
+		while (actionButtonDiv.childNodes.length) {
+		actionButtonDiv.removeChild(actionButtonDiv.lastChild);
+		}
+		actionTextDiv.innerText = "";
+	}, 300);
+}
+
+
+let actionTextDiv = document.createElement("div");
+
+for (let category of categories) {
+	let categoryDiv = document.createElement("div");
+	categoryDiv.id = category.name;
+
+
+	main.appendChild(categoryDiv);
+
+	let button = document.createElement("button");
+	let space = document.createElement("br");
+	categoryDiv.appendChild(button);
+	categoryDiv.appendChild(space);
+
+	let actionButtonDiv = document.createElement("div");
+	actionButtonDiv.className = "actionButtonDiv";
+	categoryDiv.appendChild(actionButtonDiv);
+
+
+	button.innerText = category.name.toUpperCase();
+	button.className = "categoryButton";
+
+
+	for (let action of actions) {
+		if (action.category === category.name) {
+			category.actions.push(action);
+		}
+	}
+
+	let isOpen = false;
+	button.onclick = function() {
+		let CCN = button.parentNode.id;
+		isOpen =!isOpen;
+		if (isOpen === true) {
+			let actionButtonDivs = document.querySelectorAll('.actionButtonDiv');
+			for (let ABD of actionButtonDivs) {
+				if (ABD.parentNode.id === CCN) {
+					continue;
+				}
+				closeCategory(ABD);
+			}
+			for (let action of category.actions) {
+				let actionButton = document.createElement("button");
+				
+				actionButtonDiv.appendChild(actionButton);
+				actionButton.innerText = action.name;
+				actionButton.className = "actionButton";
+				actionButton.id = action.name;
+
+				anime({
+					targets:'.actionButton',
+					opacity:1,
+					delay: anime.stagger(100)
+				})
+				anime({
+  					targets: '.actionButton',
+  					translateX: 150,
+  					delay: anime.stagger(100) // increase delay by 100ms for each elements.
+				});
+
+				let opened = false;
+
+				actionButton.onclick = function() {
+					opened = !opened;
+					if (opened) {
+						
+
+						actionTextDiv.className = "actionTextDiv";
+
+						let alts = "Can also be represented by";
+						for (let alternatives of action.text) {
+							alts += ", \""+alternatives+ "\"";
+						}
+
+						let relateds = "";
+						if (action.related.length === 0) {
+							relateds = "No related actions."
+						} else {
+							relateds = "Related to";
+							for (let related of action.related) {
+
+							}
+							relateds += ", \""+related+"\"";
+						}
+
+						let otherCats = "";
+						if (action.othercategories === "") {
+							otherCats = "No other categories."
+						} else {
+							otherCats = "Also categorized as, \""+action.othercategories+"\"";
+						}
+						
+						actionTextDiv.innerText = alts + "\n" +relateds+ "\n"+ otherCats;
+
+						let mainRect = main.getBoundingClientRect();
+						let offset = mainRect.height-actionButton.offsetTop+33;
+						let secOffset = (mainRect.left-actionButton.offsetLeft)+50;
+
+
+						
+						actionButton.dataset.vertOff=offset*-1;
+						actionButton.dataset.horOff=secOffset*-1;
+
+						console.log(actionButton.dataset.vertOff);
+						console.log(actionButton.dataset.horOff);
+
+						anime({
+							targets: '#'+action.name,
+							translateY:offset,
+							translateX: secOffset,
+							duration: 1500
+						})
+
+						anime({
+							targets:'.actionTextDiv',
+							opacity:1,
+							delay: anime.stagger(200, {start: 750})
+						})
+						anime({	
+							targets: '.actionTextDiv',
+							translateX: 60,
+							delay: anime.stagger(200, {start: 750}) // increase delay by 100ms for each elements.
+						});
+
+						actionButton.style.borderRadius = '1px';
+						actionButton.style.fontSize = 20;
+					} else {
+						console.log(actionButton.dataset.vertOff);
+						console.log(actionButton.dataset.horOff);
+
+						anime({
+							targets: '#'+action.name,
+							translateY:0,
+							translateX:150,
+							duration: 1500
+						})
+						actionButton.style.borderRadius = '20px';
+						actionButton.style.fontSize = 15;
+						anime({
+							targets:'.actionTextDiv',
+							opacity:0,
+							duration:2000
+						})
+						anime({
+							targets: '.actionTextDiv',
+							translateX:-150,
+							duration: 2000
+						})
+						window.setTimeout(function(){
+							actionTextDiv.innerText = "";
+						}, 300);
+					}
+
+					
+					
+				}
+			}
+		} else {
+			
+			closeCategory(actionButtonDiv);
+		}
+	}
+}
+
+
+footer.appendChild(actionTextDiv);
+
+
+
+
