92f8d6c71d75bf18742efed0a50d6199fbd6234c
Megna Anand <megna.anand@eduroam-169-233-164-170.ucsc.edu>
2019-07-12 11:26:10 -0700

    comitted

--- felt-sim/js/actions_megna.js
+++ felt-sim/js/actions_megna.js
@@ -255,4 +255,5 @@ actionLibrary.getIntoFight = {
       {type: 'changeAffectionLevel', affection:vars.a2, amount: -1}
     ],
     text: "😔 " + vars.n1 + " and " + vars.n2 + " got into a fight. "
+  })
 };
