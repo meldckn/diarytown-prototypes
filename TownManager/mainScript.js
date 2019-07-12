457e53b4faffd0996dda4ed9dda0bdf6f27a888f
AV <avemuri@ebay.com>
2019-07-12 15:42:15 -0700

    Merge commit

--- TownManager/mainScript.js
+++ TownManager/mainScript.js
@@ -8,6 +8,31 @@ startMenu.className = "startMenu";
 
 map.style.display = 'none';
 
+let buildings = ["assets/fireRed2.png", "assets/fireRed3.png", "assets/fireRed4.png",
+			  	 "assets/fireRed5.png", "assets/fireRed6.png", "assets/fireRed7.png"];
+
+
+let canvas = document.getElementById("canvas");
+canvas.style.marginLeft = "80px";
+canvas.style.marginTop = "40px";
+canvas.width = 1040;
+canvas.height = 700;
+let ctx = canvas.getContext("2d");
+
+let xyCanvasCoords = [ [80, 40, 130, 215],
+					   [255, 40, 240, 215],
+					   [925, 40, 190, 195]
+					 ];
+
+let randBuildingImg = new Image();
+randBuildingImg.src = buildings[Math.floor(Math.random()*buildings.length)];
+
+
+function draw() {
+	randX = Math.random() * (canvas.width - randBuildingImg.width);
+	randY = Math.random() * (canvas.height - randBuildingImg.height);
+	ctx.drawImage(randBuildingImg, randX, randY);
+}
 
 
 let startSwitch = false;
@@ -47,7 +72,7 @@ startButton.onclick = function() {
 			duration:1000
 		})
 	},500)
-	
+	draw();
 }
 
 if (startSwitch) {
