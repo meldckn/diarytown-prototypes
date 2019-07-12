4c562676c72090146bb764380b22adc8a2871fc9
kashyap <kashyap@eduroam-169-233-224-80.ucsc.edu>
2019-07-12 09:15:30 -0700

    new prototype

--- DiaryCraftUI/script.js
+++ DiaryCraftUI/script.js
@@ -31,7 +31,7 @@ window.onkeyup = function() {
 }
 */
 //get mouse coordinate on click and place circles there
-function showCoords(event) {
+/*function showCoords(event) {
   var xcoord = event.clientX;
   var ycoord = event.clientY;
   circle.style.left = xcoord;
@@ -44,7 +44,42 @@ window.onmousemove = function(event){
 
 }
 
+function allowDrop(ev) {
+  ev.preventDefault();
+}
 
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
+*/
+
+
+function clickCounter() {
+  if (typeof(Storage) !== "undefined") {
+    if (localStorage.clickcount) {
+      localStorage.clickcount = Number(localStorage.clickcount)+1;
+    } else {
+      localStorage.clickcount = 1;
+    }
+    document.getElementById("result").innerHTML = "You have clicked the button " + localStorage.clickcount + " time(s).";
+  }
+}
+
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
 
 
 /*
