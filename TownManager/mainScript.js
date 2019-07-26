f57603165f152f63a1ccb3e434184b6e25426fd8
Megna Anand <megna.anand@eduroam-169-233-164-170.ucsc.edu>
2019-07-26 14:04:26 -0700

    commited

--- TownManager/mainScript.js
+++ TownManager/mainScript.js
@@ -1011,27 +1011,14 @@ function runDiaryEditor() {
 function openCategory(categoryName) {
 	console.log(document.getElementById(categoryName).style.display === "none", document.getElementById(categoryName).style.display === "none");
 	
-	if( getComputedStyle(document.getElementById(categoryName), null).display === "none" )
+	if( getComputedStyle(document.getElementById(categoryName), null).display === "block" )
 	{		
-		// var i, tabcontent, tablinks;
-		// tabcontent = document.getElementsByClassName("tabcontent");
-		// for (i = 0; i < tabcontent.length; i++) {
-		// 	tabcontent[i].style.display = "none";
-		// }
-
-		// tablinks = document.getElementsByClassName("tablinks");
-		// for (i = 0; i < tablinks.length; i++) {
-		// 	tablinks[i].className = tablinks[i].className.replace(" active", "");
-		// }
-		
-		document.getElementById(categoryName).style.display = "block";
-		//document.getElementById(categoryName).className += " active";
+		document.getElementById(categoryName).style.display = "none";
 	}
 
 	else
 	{
-		document.getElementById(categoryName).style.display = "none";
-		//document.getElementById(categoryName).className += " active";
+		document.getElementById(categoryName).style.display = "block";
 	}
 }
 
