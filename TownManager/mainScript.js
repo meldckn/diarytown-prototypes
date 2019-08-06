f0ae24fcff6a23f3e694f4cd3e633e3a65ee84bc
AV <avemuri@ebay.com>
2019-08-06 14:42:06 -0700

    akhil

--- TownManager/mainScript.js
+++ TownManager/mainScript.js
@@ -852,10 +852,10 @@ window.setInterval(function(event) {
 	}
 	emoji.innerText = emojiQueue[0];
 	emojiQueue.shift();
-}, 1000 * 2);
+}, 1000 * 3);
 window.setInterval(function(){
 	Sim.runRandomAction();
-}, 1000 * 2);
+}, 1000 * 10);
 
 
 
